71bb624d7468274e21f1519f4f0eeb5b1b3c7ec7
Kaushal <kbhandary672@gmail.com>
2024-04-10 09:01:00 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    const position = document.documentElement.scrollTop;
+    if (position >= 200) {
+        nav.classList.add('shrink');
+        nav.classList.remove('grow');
+    } else {
+        nav.classList.remove('shrink');
+        nav.classList.add('grow');
+    }
+});
