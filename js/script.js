97fb21d857cc01d3a29f59572c55c2a0200a0cf1
Kaushal <kbhandary672@gmail.com>
2024-04-21 14:13:39 +0530

    change scroll height

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const nav = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
     const position = document.documentElement.scrollTop;
-    if (position >= 200) {
+    if (position >= 100) {
         nav.classList.add('shrink');
         nav.classList.remove('grow');
     } else {
